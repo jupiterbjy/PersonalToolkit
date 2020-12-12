2c274072f9c71cd99f0b0ff221054677751bd3bd
jupiterbjy <jupiterbjy@gmail.com>
2020-12-12 17:05:23 +0900

    Added proxy object to logger in order to add caller's name on logging. Doctest included in proxy object.

--- LoggingConfigurator.py
+++ LoggingConfigurator.py
@@ -1,4 +1,6 @@
 import logging
+import inspect
+
 
 _handler = logging.StreamHandler()
 _handler.setLevel("DEBUG")
@@ -6,3 +8,69 @@ _handler.setFormatter(logging.Formatter("[--%(levelname)s--] %(message)s"))
 LOGGER = logging.getLogger("UI_DEBUG")
 LOGGER.addHandler(_handler)
 LOGGER.setLevel("DEBUG")
+
+
+def get_caller_stack_name(depth=1):
+    """
+    Gets the name of caller.
+    :param depth: determine which scope to inspect, for nested usage.
+    """
+    return inspect.stack()[depth][3]
+
+
+def log_caller():
+    return f"<{get_caller_stack_name()}>"
+
+
+class CallerLoggedLogger:
+    """
+    >>> a = CallerLoggedLogger()
+
+    >>> a.debug('this is debug')  # prints on stderr, so no output to stdout.
+
+    >>> a.warning('this is warning')  # and so on.
+
+    >>> "debug" in dir(a)
+    True
+
+    >>> "warning" in dir(a)
+    True
+
+    >>> "critical" in dir(a)
+    False
+
+    >>> def tester():
+    ...     a.warning("<-- this guy called me!")
+    ...
+
+    >>> tester()  # testing if warning is called out properly.
+
+    """
+
+    def __init__(self, identifier=("<", ">")):
+        self.ident_left, self.ident_right = identifier
+
+    def __getattr__(self, item):
+        try:
+            target = getattr(LOGGER, item)
+        except AttributeError:
+            raise
+
+        setattr(self, item, self.set_decorated_attr(target))
+        return getattr(self, item)
+
+    def set_decorated_attr(self, logging_function, ):
+
+        def inner(msg, *args, **kwargs):
+            caller = get_caller_stack_name(depth=2)
+            logging_function(f"{self.ident_left}{caller}{self.ident_right} {msg}", *args, **kwargs)
+
+        return inner
+
+
+logger = CallerLoggedLogger()
+
+
+if __name__ == '__main__':
+    import doctest
+    doctest.testmod(verbose=True)
