18bbc492ecef462d2df5284dbb2e01b2f5f81358
jupiterbjy <jupiterbjy@gmail.com>
2021-04-06 01:55:34 +0900

    Fixed fonts not loading.

--- DynamicTaskViewer/Schedules/DelayedTester.py
+++ DynamicTaskViewer/Schedules/DelayedTester.py
@@ -14,14 +14,14 @@ class TaskObject(ScheduledTask):
         super().__init__()
         self.name = "Delay Tester"
         self.delay = 0.5
-        self._msgs = deque("가 バカにする奴は嫌いだ"
-                           "見下されるのも嫌いだ"
-                           "”アイドルなんか”という言葉を"
-                           "見てもいないくせに言うな"
-                           "頑張りを当然と言うな"
-                           "イメージとか強制するな"
-                           "”アイドルなんか”という言葉は"
-                           "この世で一番大嫌いだ".replace("\n", "  "))
+        self._msgs = deque("バカにする奴は嫌いだ  "
+                           "見下されるのも嫌いだ  "
+                           "”アイドルなんか” という言葉を  "
+                           "見てもいないくせに言うな  "
+                           "頑張りを当然と言うな  "
+                           "イメージとか強制するな  "
+                           "”アイドルなんか” という言葉は  "
+                           "この世で一番大嫌いだ")
 
     async def task(self):
         await trio.sleep(self.delay)
